4d2101ee78cec38a540d2185c7c4917fd5530b4a
Kanisaja <kanisaja.nika5@gmail.com>
2025-08-30 16:05:58 +0200

    Funkcija za dodajanje podrobnosti več vzorcem
    - in sprememba prejšnje funkcije, da pobere samo določene podatke in da doda še vrsto vzorca

--- pobiranje_podrobnosti.py
+++ pobiranje_podrobnosti.py
@@ -4,16 +4,24 @@ from pobiranje_osnov import prosnja
 def podatki_vzorca(vzorec):
     povezava = vzorec['Link']
     juha = prosnja(povezava)
+
+    vrsta_vzorca = juha.find_all('li', class_='sf-breadcrumbs__list-item')[3]
+    vzorec['Type'] = vrsta_vzorca.text.strip()
+
     tabela = juha.find_all('div', class_='sf-property--without-suffix '
                            'sf-property--value-in-middle sf-property')
     for vrstica in tabela:
-        naslov = vrstica.find('dt').text.strip()
-
-        # izključimo stvari, ki nas ne zanimajo
-        if naslov == 'Craft' or naslov == 'Notes' or \
-           naslov == 'Featured product':
+        podatek = vrstica.find('dt').text.strip()
+        podatki = ['Brand', 'Designer', 'Format', 'Language',
+                   'Number of patterns', 'Pages', 'Skill level']
+        if podatek not in podatki:
             continue
 
         vrednost = vrstica.find('dd').text.strip()
-        vzorec[naslov] = vrednost
-    return vzorec
+        vzorec[podatek] = vrednost
+
+
+def podatki_vzorcev(seznam_vzorcev):
+    for vzorec in seznam_vzorcev:
+        podatki_vzorca(vzorec)
+    return seznam_vzorcev
